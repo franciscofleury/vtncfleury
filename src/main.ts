2f412f87fbb4db8ee1f15799b4aa17972a67972e
galetti909 <joaogaletti@gmail.com>
2024-09-01 17:05:04 -0300

    fix swagger tag extra desnecessaria

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
     .setTitle('VTNC API')
     .setDescription('API para o projeto VTNC site')
     .setVersion('1.0')
-    .addTag('vtnc')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
