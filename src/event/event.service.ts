182c77f1ddcc42ebb94cd94d9dfc60d731954b84
galetti909 <joaogaletti@gmail.com>
2024-09-01 16:50:55 -0300

    fixes no service de event

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -10,57 +10,57 @@ export class EventService {
     private readonly prisma: PrismaService,
     private readonly errorHandlingService: ErrorHandlingService
   ) {}
-  create(dto: CreateEventDto) {
+  async create(dto: CreateEventDto): Promise<Event | string> {
     
     let result;
 
     try {
-      result = this.prisma.client.event.create({dto});
+      result = await this.prisma.client.event.create({dto});
     } catch (e) {
       result = this.errorHandlingService.handlePrisma(e);
     }
     return result;
   }
 
-  findAll() {
+  async findAll(): Promise<Event[] | string> {
 
     let result;
 
     try {
-      result = this.prisma.client.event.findMany();
+      result = await this.prisma.client.event.findMany();
     } catch (e) {
       result = this.errorHandlingService.handlePrisma(e);
     }
     return result;
   }
 
-  findOne(event_id: number) {
+  async findOne(event_id: number): Promise<Event | string> {
 
     let result;
 
     try {
-      const result = this.prisma.client.event.findUnique({where: {event_id: event_id}});
-    } catch (e) {
-      const result = this.errorHandlingService.handlePrisma(e);
-    }
-    return event;
-  }
-
-  update(event_id: number, updateEventDto: UpdateEventDto) {
-
-    let result;
-
-    try {
-      const result = this.prisma.client.event.update({where: {event_id: event_id}, data: updateEventDto});
+      const result = await this.prisma.client.event.findUnique({where: {event_id: event_id}});
     } catch (e) {
       const result = this.errorHandlingService.handlePrisma(e);
     }
     return result;
   }
 
-  remove(event_id: number) {
+  async update(event_id: number, updateEventDto: UpdateEventDto): Promise<Event | string> {
+
+    let result;
+
     try {
-      const event = this.prisma.client.event.delete({where: {event_id: event_id}});
+      const result = await this.prisma.client.event.update({where: {event_id: event_id}, data: updateEventDto});
+    } catch (e) {
+      const result = this.errorHandlingService.handlePrisma(e);
+    }
+    return result;
+  }
+
+  async remove(event_id: number): Promise<string> {
+    try {
+      const event = await this.prisma.client.event.delete({where: {event_id: event_id}});
     } catch (e) {
       const event = this.errorHandlingService.handlePrisma(e);
     }
