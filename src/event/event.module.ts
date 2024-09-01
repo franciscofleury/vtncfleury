dd32ebd1dcc4c69e5a92668fea0444ad4ef4dc5f
galetti909 <joaogaletti@gmail.com>
2024-09-01 17:01:45 -0300

    fix modulo event

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
+import { PrismaService } from 'src/services/prisma.service';
+import { ErrorHandlingService } from 'src/services/error_handling.service';
 
 @Module({
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, PrismaService, ErrorHandlingService],
 })
 export class EventModule {}
